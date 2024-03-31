e15e54c45d07c6e4cca30093345f7ae5f3f76eb8
lime <lime@limelime.local>
2024-03-31 19:15:15 +0200

    add bubble sort

---
+++ bubblesort.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class bubblesort {
+    public static void main(String[] args){
+        int numbers[] = new int[10];
+        Random random = new Random();
+
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = random.nextInt(100);
+        }
+        System.out.println(Arrays.toString(numbers));
+        long startTime = System.currentTimeMillis();
+
+        bubblesort(numbers);
+        
+        long endTime = System.currentTimeMillis();        
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("Took " + (endTime - startTime) + "ms");
+    }
+// first variant
+    // private static void bubblesort(int[] numbers) {
+
+    //     boolean swapped = true;
+    //     while (swapped){
+
+    //         for (int i = 0; i < numbers.length - 1; i++){
+    //             swapped = false;
+    //             if (numbers[i] > numbers[i + 1]){
+    //                 swapped = true;
+    //                 int temp = numbers[i];
+    //                 numbers[i] = numbers[i + 1];
+    //                 numbers[i + 1] = temp;
+    //             }
+    //         }
+    //     }
+    // }
+
+// second variat 
+    private static void bubblesort(int[] numbers) {
+
+            for(int i = 0; i < numbers.length - 1; i++){
+
+                for(int j = 0; j < numbers.length - i - 1; j++){
+
+                    if (numbers[j] >= numbers[j + 1]){
+                        
+                        swap(numbers, j, j + 1);
+
+                    }
+                }
+            }
+        }
+
+    private static void swap(int[] numbers, int index1, int index2) {
+
+        int temp = numbers[index1];
+        numbers[index1] = numbers[index2];
+        numbers[index2] = temp;
+    }
+}
